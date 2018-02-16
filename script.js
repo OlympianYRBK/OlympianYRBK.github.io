244905b4413bd6a8d3ac1af41a1535f0c52218ca
Christopher Bravata <thechrisbravata@gmail.com>
2018-02-16 11:30:50 -0600

    //SCRIPT 2 / Fixed formatting

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 $('.shapeframe').tilt({
-    perspective:    1000,
+    perspective:  1000,
     glare: true,
     scale: 1.05
 });
+var RPVal;
 function randPhrase() {
-  var randPhraseValue = Math.floor((Math.random() * 10) + 1);
-  if (randPhraseValue === 1) {
+  RPVal = Math.floor((Math.random() * 10) + 1);
+  deterRP(RPVal);
+};
+function deterRP(Val) {
+  if (Val === 1) {
     document.getElementById("clue").innerHTML = "It's time to go back. Way back. Back to when it all began.";
-  } else if (randPhraseValue === 2) {
-    document.getElementById("clue").innerHTML = "_____ Trojans, _____ Eagles";
-  } else if (randPhraseValue === 3) {
+  } else if (Val === 2) {
+    document.getElementById("clue").innerHTML = "Tosa _____ Trojans, _____ Eagles";
+  } else if (Val === 3) {
     document.getElementById("clue").innerHTML = "Oh the _____ smell of Spring!";
-  } else if (randPhraseValue === 4)  {
+  } else if (Val === 4)  {
     document.getElementById("clue").innerHTML = "Milwaukee Tacos";
-  } else if (randPhraseValue === 5) {
+  } else if (Val === 5) {
     document.getElementById("clue").innerHTML = "King. Queen. Princess. ?";
-  } else if (randPhraseValue === 6) {
+  } else if (Val === 6) {
     document.getElementById("clue").innerHTML = "Duh da da da dah dah dah dah da da dah";
-  } else if (randPhraseValue === 7) {
+  } else if (Val === 7) {
     document.getElementById("clue").innerHTML = "Only certain kids will remember this";
-  } else if (randPhraseValue === 8) {
+  } else if (Val === 8) {
     document.getElementById("clue").innerHTML = "This dude has two kids";
-  } else if (randPhraseValue === 9) {
+  } else if (Val === 9) {
     document.getElementById("clue").innerHTML = "______: to be warped or forced out of its natural shape. ______: the bad side of Stranger Things";
-  } else if (randPhraseValue === 10) {
+  } else if (Val === 10) {
     document.getElementById("clue").innerHTML = "Everyone's got a story, but this one is pretty famous.";
   }
-};
+}
