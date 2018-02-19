dca37ce4a92666949165bd38f8c4d4a32a7645d3
Christopher Bravata <thechrisbravata@gmail.com>
2018-02-19 17:01:57 -0600

    + Purchase Button, Deadline Notice
    Added a purchase button and a notice indicating when the last day is to
    purchase a yearbook for the 2017-18 year.

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 $('.shapeframe').tilt({
-    perspective:    1000,
+    perspective: 1000,
     glare: true,
     scale: 1.05
 });
+
 function randPhrase() {
-  deterRP(Math.floor((Math.random() * 10) + 1;
+    deterRP(Math.floor((Math.random() * 10) + 1));
 };
+
 function deterRP(Val) {
-  if (Val === 1) {
-    document.getElementById("clue").innerHTML = "It's time to go back. Way back. Back to when it all began.";
-  } else if (Val === 2) {
-    document.getElementById("clue").innerHTML = "Tosa _____ Trojans, _____ Eagles";
-  } else if (Val === 3) {
-    document.getElementById("clue").innerHTML = "Oh the _____ smell of Spring!";
-  } else if (Val === 4)  {
-    document.getElementById("clue").innerHTML = "Milwaukee Tacos";
-  } else if (Val === 5) {
-    document.getElementById("clue").innerHTML = "King. Queen. Princess. ?";
-  } else if (Val === 6) {
-    document.getElementById("clue").innerHTML = "Duh da da da dah dah dah dah da da dah";
-  } else if (Val === 7) {
-    document.getElementById("clue").innerHTML = "Only certain kids will remember this";
-  } else if (Val === 8) {
-    document.getElementById("clue").innerHTML = "This dude has two kids";
-  } else if (Val === 9) {
-    document.getElementById("clue").innerHTML = "______: to be warped or forced out of its natural shape. ______: the bad side of Stranger Things";
-  } else if (Val === 10) {
-    document.getElementById("clue").innerHTML = "Everyone's got a story, but this one is pretty famous.";
-  }
-}
+    if (Val === 1) {
+        document.getElementById("clue").innerHTML = "It's time to go back. Way back. Back to when it all began.";
+    } else if (Val === 2) {
+        document.getElementById("clue").innerHTML = "Tosa _____ Trojans, _____ Eagles";
+    } else if (Val === 3) {
+        document.getElementById("clue").innerHTML = "Oh the _____ smell of Spring!";
+    } else if (Val === 4) {
+        document.getElementById("clue").innerHTML = "Milwaukee Tacos";
+    } else if (Val === 5) {
+        document.getElementById("clue").innerHTML = "King. Queen. Princess. ?";
+    } else if (Val === 6) {
+        document.getElementById("clue").innerHTML = "Duh da da da dah dah dah dah da da dah";
+    } else if (Val === 7) {
+        document.getElementById("clue").innerHTML = "Only certain kids will remember this";
+    } else if (Val === 8) {
+        document.getElementById("clue").innerHTML = "This dude has two kids";
+    } else if (Val === 9) {
+        document.getElementById("clue").innerHTML = "______: to be warped or forced out of its natural shape. ______: the bad side of Stranger Things";
+    } else if (Val === 10) {
+        document.getElementById("clue").innerHTML = "Everyone's got a story, but this one is pretty famous.";
+    }
+};
